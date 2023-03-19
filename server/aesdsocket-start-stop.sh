c050edd0cb92f4c6860f91d5e158b92492ecac83
admin <admin@MARK-PC-VM.myguest.virtualbox.org>
2023-03-19 18:04:36 -0400

    remove aarch64 naming

--- server/aesdsocket-start-stop.sh
+++ server/aesdsocket-start-stop.sh
@@ -3,12 +3,12 @@
 #taken directly from Linux System Initialization, 3:35
 case "$1" in
     start)
-        echo "Starting aarch64-aesdsocket"
-        start-stop-daemon -S -n aarch64-aesdsocket -a /usr/bin/aarch64-aesdsocket -- -d
+        echo "Starting aesdsocket"
+        start-stop-daemon -S -n aesdsocket -a /usr/bin/aesdsocket -- -d
         ;;
     stop)
-        echo "Stoping aarch64-aesdsocket"
-        start-stop-daemon -K -n aarch64-aesdsocket
+        echo "Stoping aesdsocket"
+        start-stop-daemon -K -n aesdsocket
         ;;
     *)
         echo "Usage: $0 {start|stop}"

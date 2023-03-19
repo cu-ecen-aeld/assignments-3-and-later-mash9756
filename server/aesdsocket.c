717c0642e2198c8cb04e761f0eefd7f99e7733ba
admin <admin@MARK-PC-VM.myguest.virtualbox.org>
2023-03-19 19:33:18 -0400

    removing mutex lock for debug

--- server/aesdsocket.c
+++ server/aesdsocket.c
@@ -152,19 +152,19 @@ static int process_recv_pkt(char **pkt, int clientFD, pthread_mutex_t *mutex, lo
     fd = open(OUTPUT_FILE, O_RDWR|O_CREAT|O_APPEND, S_IRWXU|S_IRWXG|S_IRWXO);
 
     printf("\n%ld bytes to be written: %s,\n\n", len, *pkt);
-    printf("\nlocking\n");
-    if(pthread_mutex_lock(mutex))
-    {
-        printf("\nfailed to lock");
-        free(readback);
-        close(fd);
-        return -1;
-    }
+    // printf("\nlocking\n");
+    // if(pthread_mutex_lock(mutex))
+    // {
+    //     printf("\nfailed to lock");
+    //     free(readback);
+    //     close(fd);
+    //     return -1;
+    // }
     
     printf("\nwriting\n");
     write(fd, *pkt, len);
-    printf("\nunlocking\n");
-    pthread_mutex_unlock(mutex);
+    // printf("\nunlocking\n");
+    // pthread_mutex_unlock(mutex);
 
     printf("\nSeeking to start of file\n");
 /* seek to file start for readback */
